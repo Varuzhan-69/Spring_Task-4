420b31f28b8a5cc2512f63c947350210efbb0064
Varuzhan Manucharyan <varuzh.manucharian@yandex.ru>
2025-05-22 13:35:47 +0300

    Выполнил задание 2.1.4

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,12 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+    @Override
+    public String toString() {
+        return " P.S. Последний бин (В Idea написано на нерусском языке, оставил не тронутым)";
+    }
 }
